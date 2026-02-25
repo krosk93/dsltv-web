e19e9f3f08e322e771a45c10cdc408a9010ad02f
Oscar Rovira <560186+krosk93@users.noreply.github.com>
2026-02-25 20:20:05 +0100

    Fix locale

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,8 @@
 import { getRequestConfig } from 'next-intl/server';
 
-export default getRequestConfig(async ({ requestLocale }) => {
-    const locale = await requestLocale;
+export default getRequestConfig(async ({ locale }) => {
     return {
-        locale,
+        locale: locale || 'ca',
         messages: (await import(`../messages/${locale}.json`)).default
     };
 });
